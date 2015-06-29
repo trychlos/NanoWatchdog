b1845b8432cd96bd1e7656b8f07b9a0c42bce546
Pierre Wieser <pwieser@trychlos.org>
2015-06-29 06:44:53 +0200

    Bump TCP client version number

--- src/client/nw-client.pl
+++ src/client/nw-client.pl
@@ -59,7 +59,7 @@ sub msg_help(){
 
 # ---------------------------------------------------------------------
 sub msg_version(){
-	print ' NanoWatchdog v2015.1
+	print ' NanoWatchdog v2015.2
  Copyright (C) 2015, Pierre Wieser <pwieser@trychlos.org>
 ';
 }
